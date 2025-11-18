23ec0b7057d3dfa26c729b89dbcd2d2879a55011
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-11-18 20:16:00 +0100

    Revert "chore: drop yml extension support"
    This reverts commit 781c7a216adfb83837adc8952985ae216ca43f90.

--- packages/typescript/tests/binding/mime-utils.spec.ts
+++ packages/typescript/tests/binding/mime-utils.spec.ts
@@ -76,6 +76,7 @@ describe("MIME Utility Functions", () => {
 			const testCases = [
 				{ ext: "json", expected: "application/json" },
 				{ ext: "yaml", expected: "application/x-yaml" },
+				{ ext: "yml", expected: "application/x-yaml" },
 				{ ext: "xml", expected: "application/xml" },
 				{ ext: "csv", expected: "text/csv" },
 				{ ext: "toml", expected: "application/toml" },
