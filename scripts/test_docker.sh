3bfa701f839a0446919ca820ae07a1a5aa3ed8d5
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-12-12 20:05:00 +0100

    fix(scripts): resolve shellcheck SC2317 warning in cleanup function
    The cleanup function is invoked indirectly via trap on exit. Added SC2317
    disable comment alongside existing SC2329 to suppress false positive about
    unreachable code in the subshell.

--- scripts/test_docker.sh
+++ scripts/test_docker.sh
@@ -122,7 +122,7 @@ fail_test() {
 }
 
 # Cleanup function
-# shellcheck disable=SC2329
+# shellcheck disable=SC2317,SC2329
 cleanup() {
 	log_info "Cleaning up test containers..."
 	docker ps -a --filter "name=${CONTAINER_PREFIX}" --format "{{.Names}}" | while read -r container; do
